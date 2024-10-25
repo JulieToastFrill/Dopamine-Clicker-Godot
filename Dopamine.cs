31fc069391c57458485ce894cf0e4f3e11ccc402
JulieToastFrill <53793305+JulieToastFrill@users.noreply.github.com>
2024-10-25 13:35:35 -0400

    Initial push

---
+++ Dopamine.cs
@@ -0,0 +1,22 @@
+using Godot;
+using System;
+
+public partial class Dopamine : Node3D
+{
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+	{
+		//var Transform = new Transform3D();
+		//GD.Print(Transform);
+		
+
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+  //      var Transform = new Transform3D();
+		//GD.Print(Transform);
+
+    }
+}
